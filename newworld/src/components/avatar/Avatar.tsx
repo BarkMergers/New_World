2507bcaa94c953b9b23f38af0e8e731e2ffa672f
Mark Burgess <mburgess1983@gmail.com>
2025-08-26 14:34:09 +0100

    Change icon

--- newworld/src/components/avatar/Avatar.tsx
+++ newworld/src/components/avatar/Avatar.tsx
@@ -1,16 +1,11 @@
-
-
-
+import AvatarIcon from '/genericAvatar.png'
 
 export default function Avatar({ size = 16 }: { size?: number }) {
-
     return (
         <div className="avatar">
             <div className={`rounded-full border border-gray-400`} style={{ height: size + "rem", width: size + "rem"}} >
-                <img src="https://img.daisyui.com/images/profile/demo/distracted2@192.webp" />
+                <img src={AvatarIcon} />
             </div>
         </div>
     )
 }
-
-
