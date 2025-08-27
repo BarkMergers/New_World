b94a158c3e1797f513340f1234b36a86d814474a
Mark Burgess <mburgess1983@gmail.com>
2025-08-27 21:58:24 +0100

    portal

--- newworld/src/App.tsx
+++ newworld/src/App.tsx
@@ -12,6 +12,7 @@ import { globalData, UserContext } from "./helpers/globalData";
 import SpinnerLoader from "./elements/spinnerLoader/SpinnerLoader";
 import { HandleLogout, HandleLogin } from "./helpers/signin";
 import Modal from "./components/modal/Modal";
+import { LogoutClose } from "./helpers/logout/Logout";
 
 
 const Home = lazy(() => import('./screens/home/home/Home'));
@@ -48,6 +49,7 @@ function App() {
                         <Route path="/customer/:id" element={<CustomerDetails />}></Route>
                         <Route path="/plain" element={<Plain accounts={accounts} />}></Route>
                         <Route path="/logout" element={<Logout />}></Route>
+                        <Route path="/logout-close" element={<LogoutClose />}></Route>
                         <Route path="/" element={<Dashboard />}></Route>
                     </Routes>
                 </div>
--- newworld/src/helpers/logout/Logout.tsx
+++ newworld/src/helpers/logout/Logout.tsx
@@ -4,10 +4,23 @@ import { useMsal } from "@azure/msal-react";
 export default function Logout() {
     const { instance } = useMsal();
 
-    
     useEffect(() => {
-        instance.logoutRedirect();
+        instance.logoutRedirect({
+            postLogoutRedirectUri: `${window.location.origin}/logout-close`
+        });
     }, [instance]);
 
     return <p>Auto Logging out...</p>;
+}
+
+
+export function LogoutClose() {
+    useEffect(() => {
+        // Give the logout redirect a moment, then close this tab
+        setTimeout(() => {
+            window.close();
+        }, 500);
+    }, []);
+
+    return <p>You have been logged out. This tab will close.</p>;
 }
